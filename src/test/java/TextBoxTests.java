bef5b7912d50d66243b2a0a744f27cba1aa8247f
Ilia <ilia19011991@gmail.com>
2022-11-21 21:04:45 +0300

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+
+        String userName ="Ilia";
+        String userEmail ="ilia@gmail.com";
+        String currentAddress ="Address 1";
+        String permanentAddress ="Address 2";
+
+        open("/text-box");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output").shouldBe(Condition.visible);
+
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+    }
+}
